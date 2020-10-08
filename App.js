05aa1a47ea9532ac886b81ee81534e84689b115b
jbebar <jubarbe31@gmail.com>
2020-10-08 20:01:01 +0200

    Refactor and add some style

---
+++ App.js
@@ -0,0 +1,47 @@
+async function fetchRecipes() {
+  console.log(location.hostname);
+  const recipesHost = location.hostname === "localhost" ? "http://localhost:3000" : "https://jbebar.github.io/in-my-fridge";
+  return await fetch(`${recipesHost}/recipes.json`)
+    .then((r) => r.json())
+    .then((d) => d.map((e) => e.name));
+}
+
+function RecipeList(props) {
+  const lines = props.names.map((n) => <RecipeItem id={n} name={n} />);
+  const recipeTableStyle = {
+    lineHeight: 2,
+    width: "300px",
+  };
+  return <div style={recipeTableStyle}> {lines} </div>;
+}
+
+function RecipeItem(props) {
+  const recipeStyle = {
+    fontSize: 18,
+    border: "2px solid",
+    margin: "10px",
+    fontFamily: "cursive",
+    borderRadius: "5px",
+    textAlign: "center",
+  };
+  return (
+    <div id={props.name} style={recipeStyle}>
+      {props.name}
+    </div>
+  );
+}
+
+function App() {
+  const [recipes, setRecipes] = React.useState([]);
+
+  React.useEffect(() => {
+    const initRecipes = async () => {
+      setRecipes(await fetchRecipes());
+    };
+    initRecipes();
+  }, [fetchRecipes, setRecipes]);
+  return <RecipeList names={recipes} />;
+}
+
+const rootContainer = document.querySelector("#root");
+ReactDOM.render(<App />, rootContainer);
